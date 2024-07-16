d6942db50771f83bafeaf0df9daa2f2532d58e6e
Julian Rozo <jfrozosanchez@gmail.com>
2024-07-16 17:39:12 -0500

    mi primer portafolio con JavaScript

---
+++ form/customErrors.js
@@ -0,0 +1,28 @@
+export const tiposError = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "tooShort",
+];
+export const mensajes = {
+  nombre: {
+    valueMissing: "El campo nombre no puede estar vacío.",
+    patternMismatch: "Por favor, ingrese un nombre válido.",
+    tooShort: "Por favor, ingrese un nombre válido.",
+  },
+  email: {
+    valueMissing: "El campo email no puede estar vacío.",
+    typeMismatch: "Por favor, ingrese un email válido.",
+    tooShort: "Por favor, ingrese un e-mail válido.",
+  },
+  asunto: {
+    valueMissing: "El campo asunto no puede estar vacío.",
+    patternMismatch: "Por favor, ingrese el asunto de su mensaje.",
+    tooShort: "El asunto es demasiado corto.",
+  },
+  mensaje: {
+    valueMissing: "El campo mensaje no puede estar vacío.",
+    patternMismatch: "Por favor, ingrese su mensaje.",
+    tooShort: "El campo mensaje es muy corto.",
+  }
+};
---
+++ main.js
@@ -0,0 +1,8 @@
+const github = document.querySelector(".github")
+
+
+
+github.addEventListener("click", ()=> {
+    window.location.href="https://github.com/Julian-Rozo"
+})
+
